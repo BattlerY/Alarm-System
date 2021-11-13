e6fbb5f3feb883d5d76693467088e1d01d01da23
BattlerY <93649501+BattlerY@users.noreply.github.com>
2021-11-13 12:56:30 +0300

    2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,18 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{https://github.com/BattlerY/Alarm-System/blob/main/Assets/Scripts/Player.cs
     [SerializeField] private float _speed;
 
     private void Update()
     {
         transform.Translate(transform.right * _speed * Time.deltaTime * Input.GetAxis("Horizontal"));
         transform.Translate(transform.up * _speed * Time.deltaTime * Input.GetAxis("Vertical"));
-    }
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-      
     }
 }
